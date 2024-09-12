93f64b438372b2921ca7f934cf00589dfa3dfc1c
Song Yang <200042165@fvtc.edu>
2024-09-11 19:28:16 -0700

    created tip calculator

---
+++ TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+
+        // Handler for 15% tip calculation
+        private void OnTip15Clicked(object sender, EventArgs e)
+        {
+            CalculateTip(0.15);
+        }
+
+
+
+        // Handler for 20% tip calculation
+        private void OnTip20Clicked(object sender, EventArgs e)
+        {
+            CalculateTip(0.20);
+        }
+
+
+
+        // Handler for 25% tip calculation
+        private void OnTip25Clicked(object sender, EventArgs e)
+        {
+            CalculateTip(0.25);
+        }
+
+
+
+        // Method to calculate tip
+        private void CalculateTip(double tipPercentage)
+        {
+            if (double.TryParse(BillAmountEntry.Text, out double billAmount))
+            {
+                double tipAmount = billAmount * tipPercentage;
+                double totalAmount = billAmount + tipAmount;
+
+
+
+                // Update result label
+                ResultLabel.Text = $"%{tipPercentage * 100:F0} Tip: ${tipAmount:F2}   |   Total: ${totalAmount:F2}";
+            }
+            else
+            {
+                ResultLabel.Text = "Please enter a valid bill amount.";
+            }
+        }
+    }
+}
